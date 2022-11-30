cb0dea11e59cf675b8f719059480338b9266e459
dmmitr632 <df6tb43@yandex.ru>
2022-11-30 13:43:13 +0300

    fix: fix Films Likes Comparator

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmLikesReversedComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmLikesReversedComparator.java
@@ -6,13 +6,12 @@ public class FilmLikesReversedComparator implements Comparator<Film> {
     @Override
     public int compare(Film film1, Film film2) {
 
-        //        if (film1.getUsersWhoLikedFilm().size() > film2.getUsersWhoLikedFilm().size()) {
-        //            return -1;
-        //        } else if (film1.getUsersWhoLikedFilm().size() < film2.getUsersWhoLikedFilm().size()) {
-        //            return 1;
-        //        } else {
-        //            return 0;
-        //        }
-        return (0);
+        if (film1.getRate() > film2.getRate()) {
+            return -1;
+        } else if (film1.getRate() < film2.getRate()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
